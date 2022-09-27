fade9f8186a89e79259a598b7566cf0929f281df
fathanbk <fathanbk08@gmail.com>
2022-09-27 21:34:13 +0700

    Ionic Synched & Updated

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Author Summary</ion-title>
+        <ion-title>Author Biography</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true" >
